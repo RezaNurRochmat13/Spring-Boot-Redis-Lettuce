abf9a58ad647faad0fc2da93dbd944384ffb3cad
Reja <rezanurrochmat@gmail.com>
2022-02-25 13:38:24 +0700

    Add redis cluster config

--- src/main/java/com/redis/clustered/config/RedisConfig.java
+++ src/main/java/com/redis/clustered/config/RedisConfig.java
@@ -6,15 +6,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static io.lettuce.core.ReadFrom.REPLICA_PREFERRED;
 
 @Configuration
 @EnableConfigurationProperties(RedisProperties.class)
@@ -28,20 +25,17 @@ public class RedisConfig {
     @Value("${spring.redis.nodes[2]}")
     private String redisThreeNodes;
 
+    @Value("${spring.redis.host")
+    private String redisHost;
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
+    public JedisConnectionFactory redisConnectionFactory() {
         List<String> clusterNodes = new ArrayList<>();
         clusterNodes.add(redisFirstNodes);
         clusterNodes.add(redisTwoNodes);
         clusterNodes.add(redisThreeNodes);
 
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .readFrom(REPLICA_PREFERRED)
-                .build();
-
-        RedisClusterConfiguration serverConfig = new RedisClusterConfiguration(clusterNodes);
-
-        return new LettuceConnectionFactory(serverConfig, clientConfig);
+        return new JedisConnectionFactory(new RedisClusterConfiguration(clusterNodes));
     }
 
     @Bean
